d680d6effec57c35e5a75a8f10ddf8394175ff3f
Hitusss <kuba.niespielak@gmail.com>
2024-09-25 11:48:13 +0200

    feat(BigWigs): update profile data

--- data/BigWigs.lua
+++ data/BigWigs.lua
@@ -6,50 +6,54 @@ Addon.data.BigWigs = {
     ["BigWigs_Plugins_AutoReply"] = {},
     ["BigWigs_Plugins_Countdown"] = {},
     ["BigWigs_Plugins_AltPower"] = {},
-    ["BigWigs_Bosses_Ki'katal the Harvester"] = {},
     ["BigWigs_Plugins_Colors"] = {},
-    ["BigWigs_Plugins_Raid Icons"] = {},
-    ["BigWigs_Plugins_InfoBox"] = {},
+    ["BigWigs_Plugins_Wipe"] = {},
+    ["BigWigs_Plugins_Nameplates"] = {
+      ["profiles"] = {
+        ["HitusUI"] = {
+          ["iconOffsetY"] = -4,
+        },
+      },
+    },
     ["BigWigs_Plugins_Bars"] = {
       ["profiles"] = {
         ["HitusUI"] = {
           ["emphasizeGrowup"] = true,
-          ["expWidth"] = 180,
+          ["expWidth"] = 190,
           ["growup"] = true,
           ["expPosition"] = {
             nil,
             nil,
             315,
-            -149,
+            -143,
           },
+          ["normalWidth"] = 270,
           ["normalPosition"] = {
             "RIGHT",
             "RIGHT",
-            -367,
-            208,
+            0,
+            -55,
           },
         },
       },
     },
-    ["BigWigs_Bosses_Anub'zekt"] = {},
     ["BigWigs_Bosses_Avanoxx"] = {},
     ["BigWigs_Bosses_Ara-Kara, City of Echoes Trash"] = {},
-    ["BigWigs_Plugins_Wipe"] = {},
-    ["BigWigs_Plugins_Statistics"] = {},
-    ["BigWigs_Plugins_Nameplates"] = {
-      ["profiles"] = {
-        ["HitusUI"] = {
-          ["updated"] = true,
-          ["iconOffsetY"] = -4,
-        },
-      },
-    },
-    ["BigWigs_Plugins_Messages"] = {},
-    ["BigWigs_Plugins_Proximity"] = {},
-    ["BigWigs_Plugins_Pull"] = {},
     ["LibDualSpec-1.0"] = {},
+    ["BigWigs_Plugins_InfoBox"] = {},
+    ["BigWigs_Plugins_Raid Icons"] = {},
+    ["BigWigs_Plugins_Proximity"] = {},
+    ["BigWigs_Plugins_Messages"] = {},
+    ["BigWigs_Plugins_Pull"] = {},
+    ["BigWigs_Plugins_Statistics"] = {},
   },
   ["profileKeys"] = {},
+  ["global"] = {
+    ["watchedMovies"] = {
+      [-2296] = true,
+      [-2292] = true,
+    },
+  },
   ["profiles"] = {
     ["HitusUI"] = {},
   },
