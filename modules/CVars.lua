530ca15b0948b74638bf086d85f020c8861fbe56
Hitusss <kuba.niespielak@gmail.com>
2024-11-11 18:55:53 +0100

    feat(CVars): add character highlight and other missing settings

--- modules/CVars.lua
+++ modules/CVars.lua
@@ -1,32 +1,40 @@
 local name = "CVars"
 
 local function setupCVars()
-  SetCVar("cameraDistanceMaxZoomFactor", 2.6)
-  SetCVar("CameraReduceUnexpectedMovement", 1)
   SetCVar("playIntroMovie", 0)
-
-  SetCVar("ResampleAlwaysSharpen", 1)
-
-  SetCVar("autoLootDefault", 1)
 
   SetCVar("showTutorials", 0)
   SetCVar("showNPETutorials", 0)
-
   SetCVar("threatWarning", 3)
 
   SetCVar("guildMemberNotify", 0)
   SetCVar("showToastOnline", 0)
   SetCVar("showToastOffline", 0)
 
+  SetCVar("ResampleAlwaysSharpen", 1)
+
+  SetCVar("cameraDistanceMaxZoomFactor", 2.6)
+  SetCVar("CameraReduceUnexpectedMovement", 1)
+  SetCVar("CameraKeepCharacterCentered", 0)
+  SetCVar("ShakeStrengthCamera", 0.25)
+  SetCVar("ShakeStrengthUI", 0.25)
+
   SetCVar("enableMouseoverCast", 1)
+
+  SetCVar("findYourselfAnywhere", 1)
+  SetCVar("findYourselfModeCircle", 1)
+  SetCVar("findYourselfModeOutline", 1)
+  SetCVar("occludedSilhouettePlayer", 1)
+
+  SetCVar("autoLootDefault", 1)
 
   SetCVar("enableMultiActionBars", 15)
   SetCVar("AutoPushSpellToActionBar", 0)
   SetCVar("lockActionBars", 1)
   SetCVar("countdownForCooldowns", 1)
 
-  SetCVar("statusText", 1)
-  SetCVar("statusTextDisplay", "BOTH")
+  SetCVar("statusText", 0)
+  SetCVar("statusTextDisplay", "NONE")
   SetCVar("showTargetOfTarget", 1)
 
   SetCVar("cursorSizePreferred", 1)
