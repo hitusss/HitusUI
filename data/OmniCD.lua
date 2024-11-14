332c86d082f55a65d306bca87e8a4ec171aa5647
Hitusss <kuba.niespielak@gmail.com>
2024-11-14 21:18:44 +0100

    feat(OmniCD): make icons bigger and add bars 1-5

--- data/OmniCD.lua
+++ data/OmniCD.lua
@@ -2,15 +2,112 @@ Addon.data.OmniCD = {
   ["Party"] = {
     ["party"] = {
       ["extraBars"] = {
-        ["raidBar1"] = {
+        ["raidBar5"] = {
+          ["truncateIconName"] = 4,
+          ["progressBar"] = false,
+          ["enabled"] = true,
+          ["layout"] = "horizontal",
           ["manualPos"] = {
-            ["raidBar1"] = {
-              ["y"] = 354.4537420773413,
-              ["x"] = 4.480439352979374,
+            ["raidBar5"] = {
+              ["y"] = 701.1199062037485,
+              ["x"] = 222.6135106492002,
             },
           },
+          ["scale"] = 1,
           ["locked"] = true,
+          ["sortBy"] = 9,
         },
+        ["raidBar2"] = {
+          ["truncateIconName"] = 4,
+          ["progressBar"] = false,
+          ["enabled"] = true,
+          ["layout"] = "horizontal",
+          ["manualPos"] = {
+            ["raidBar2"] = {
+              ["y"] = 627.5201422238315,
+              ["x"] = 222.6131981492072,
+            },
+          },
+          ["scale"] = 1,
+          ["locked"] = true,
+          ["sortBy"] = 9,
+        },
+        ["raidBar3"] = {
+          ["truncateIconName"] = 4,
+          ["progressBar"] = false,
+          ["enabled"] = true,
+          ["layout"] = "horizontal",
+          ["manualPos"] = {
+            ["raidBar3"] = {
+              ["y"] = 676.5868598771049,
+              ["x"] = 222.6137059616958,
+            },
+          },
+          ["scale"] = 1,
+          ["locked"] = true,
+          ["sortBy"] = 9,
+        },
+        ["raidBar1"] = {
+          ["statusBarWidth"] = 185,
+          ["locked"] = true,
+          ["manualPos"] = {
+            ["raidBar1"] = {
+              ["y"] = 355.5203826785,
+              ["x"] = 223.1470848560239,
+            },
+          },
+          ["scale"] = 0.6000000000000001,
+        },
+        ["raidBar4"] = {
+          ["truncateIconName"] = 4,
+          ["progressBar"] = false,
+          ["enabled"] = true,
+          ["layout"] = "horizontal",
+          ["manualPos"] = {
+            ["raidBar4"] = {
+              ["y"] = 652.0533057379726,
+              ["x"] = 222.6135301804497,
+            },
+          },
+          ["scale"] = 1,
+          ["locked"] = true,
+          ["sortBy"] = 9,
+        },
+      },
+      ["spells"] = {
+        ["*"] = false,
+      },
+      ["icons"] = {
+        ["scale"] = 0.9000000000000001,
+      },
+      ["position"] = {
+        ["layout"] = "doubleRow",
+      },
+      ["frame"] = {
+        ["other"] = 5,
+        ["freedom"] = 3,
+        ["disarm"] = 4,
+        ["cc"] = 4,
+        ["raidMovement"] = 5,
+        ["immunity"] = 3,
+      },
+      ["spellGlow"] = {
+        ["*"] = false,
+      },
+    },
+    ["arena"] = {
+      ["icons"] = {
+        ["scale"] = 0.9000000000000001,
+      },
+      ["position"] = {
+        ["layout"] = "doubleRow",
+      },
+    },
+  },
+  ["General"] = {
+    ["fonts"] = {
+      ["icon"] = {
+        ["size"] = 12,
       },
     },
   },
